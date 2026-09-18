7a6c8cb65680b134bbabd71d5120209b28870c3c
koushiksai166 <saikoushik166@gmail.com>
2026-09-18 15:57:58 +0530

    upadted the main.ts with frontend url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   app.use('/uploads', express.static(uploadsDir));
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173,https://martirmony-frontend.vercel.app",
     credentials: true,
   });
 
